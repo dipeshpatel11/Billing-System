b969af8e9aff1ab1cdbd919ee93a495b0a7e6470
Dipesh Patel <117682312+dipeshpatel11@users.noreply.github.com>
2023-01-15 17:35:10 -0700

    Add files via upload

---
+++ Hotel_Billing/src/main/java/Hotel_Billing.java
@@ -0,0 +1,23 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author deeppatel
+ */
+public class Hotel_Billing {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CustomerBill().setVisible(true);
+            }
+        });
+
+    }
+}
